bfa25f81f9b69959101dd07bb087716fef7f9b1d
Picorique <meupau@hotmail.com>
2018-06-15 16:34:28 -0300

    Macaco Pinto

---
+++ src/Point.java
@@ -0,0 +1,20 @@
+public class Point {
+
+    public Point (int x, int y) {
+        this.x = x;
+        this.y = y;
+
+    }
+
+    public int x(){
+        return x;
+    }
+
+    public int y() {
+        return y;
+    }
+
+    private int x;
+    private int y;
+
+}
